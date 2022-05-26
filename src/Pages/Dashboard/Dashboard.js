0f2aea22a9a8b5ccf3200212f4014228857c8bee
Muhsin Khan <mdmuhsinuddin01@gmail.com>
2022-05-26 23:42:33 +0600

    finalization.

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -14,7 +14,7 @@ const Dashboard = () => {
           for="my-drawer-2"
           class="btn btn-primary drawer-button lg:hidden"
         >
-          Open drawer
+          Open Drawer
         </label>
       </div>
       <div class="drawer-side">
