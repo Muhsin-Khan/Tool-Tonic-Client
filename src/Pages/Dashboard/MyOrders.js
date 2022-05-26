3cfd66b553ab70a1d3702ce8b724366ae4c6f510
Muhsin Khan <mdmuhsinuddin01@gmail.com>
2022-05-26 20:12:33 +0600

    my order done

--- src/Pages/Dashboard/MyOrders.js
+++ src/Pages/Dashboard/MyOrders.js
@@ -15,7 +15,39 @@ const MyOrders = () => {
     }
   },[user])
   return (
-    <div>MyOrders : {orders.length}</div>
+    <div>
+      <h1 className='text-lg text-center my-3 font-semibold'>My Total Orders : {orders.length}</h1>
+    <div class="overflow-x-auto">
+  <table class="table w-full">
+    {/* <!-- head --> */}
+    <thead>
+      <tr>
+        <th>Index</th>
+        <th>Product Name</th>
+        <th>Ordered ID</th>
+        <th>Orderd Quantity</th>
+        <th>Customer's Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        orders.map((o, index) => <tr>
+          <th>{index+1}</th>
+          <td>{o.productName}</td>
+          <td>{o.productId}</td>
+          <td>{o.productQuantity}</td>
+          <td>{o.customerEmail}</td>
+          
+        </tr> )
+      }
+      
+      
+      
+    </tbody>
+  </table>
+</div>
+    
+    </div>
   )
 }
 
